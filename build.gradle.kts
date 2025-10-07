5ebdc16a5cfbe3e54b23807ba57d1c3907ae07fa
barlos <9cguizar2004@gmail.com>
2025-10-07 13:08:03 -0700

    springboooottt

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+springBoot {
+    mainClass.set("com.example.springboot.Application")
+}
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
